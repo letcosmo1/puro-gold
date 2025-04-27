93062870726dd89f150643e9e2cd9151aaa3a090
Leticia Cosmo <letcosmogeller@gmail.com>
2025-04-27 18:16:06 -0300

    added add client dialog

--- app/page.tsx
+++ app/page.tsx
@@ -11,60 +11,85 @@ import { Settings2 } from "lucide-react";
 import { useState } from "react";
 
 export default function Home() {
-  const [open, setOpen] = useState(false);
+  const [openEditClient, setOpenEditClient] = useState(false);
+  const [openAddClient, setOpenAddClient] = useState(false);
 
   const handleEditClientButtonClick = () => {
-    setOpen(true);
+    setOpenEditClient(true);
+  }
+
+  const handleAddClientButtonClick = () => {
+    setOpenAddClient(true);
   }
 
   return (
-    <div className="flex flex-col justify-between h-[calc(100vh-var(--header-height)-var(--spacing)*8)]">
-      <div>
-        <Label htmlFor="search-client" className="mb-2">Pesquisar</Label>
-        <Input id="search-client" placeholder="Nome do cliente" />
+    <>
+      <div className="flex flex-col justify-between h-[calc(100vh-var(--header-height)-var(--spacing)*8)]">
+        <div>
+          <Label htmlFor="search-client" className="mb-2">Pesquisar</Label>
+          <Input id="search-client" placeholder="Nome do cliente" />
+        </div>
+
+        <div className="rounded-md border p-4 h-[calc(100%-94px-var(--spacing)*8)]">
+          <h2 className="font-medium text-sm mb-2">Clientes</h2>
+          <ScrollArea className="w-full h-[calc(100%-20px)]">
+            <Table>
+              <TableBody>
+                {
+                  clients.map(client => { 
+                    return (
+                      <TableRow key={ client.id }>
+                        <TableCell className="flex justify-between py-4">
+                          <p>{ client.name }</p>
+                          <Settings2 onClick={ handleEditClientButtonClick } />
+                        </TableCell>
+                      </TableRow>
+                    )
+                  })
+                }
+              </TableBody>
+            </Table>
+          </ScrollArea>
+        </div>
+
+        <Button onClick={ handleAddClientButtonClick } className="w-full">Adicionar Cliente</Button>
       </div>
 
-      <div className="rounded-md border p-4 h-[calc(100%-94px-var(--spacing)*8)]">
-        <h2 className="font-medium text-sm mb-2">Clientes</h2>
-        <ScrollArea className="w-full h-[calc(100%-20px)]">
-          <Table>
-            <TableBody>
-              {
-                clients.map(client => { 
-                  return (
-                    <TableRow key={ client.id }>
-                      <TableCell className="flex justify-between py-4">
-                        <p>{ client.name }</p>
-                        <Settings2 onClick={ handleEditClientButtonClick } />
-                      </TableCell>
-                    </TableRow>
-                  )
-                })
-              }
-            </TableBody>
-          </Table>
-        </ScrollArea>
-      </div>
-
-      <Button className="w-full">Adicionar Cliente</Button>
-
-      <Dialog open={ open } onOpenChange={ setOpen }>
+      <Dialog open={ openEditClient } onOpenChange={ setOpenEditClient }>
         <DialogTrigger />
-        
+
         <DialogContent>
           <DialogHeader>
             <DialogTitle>Editar nome do cliente</DialogTitle>
           </DialogHeader>
           <div>
-            <Label htmlFor="client-name" className="mb-2">Novo nome</Label>
-            <Input id="client-name" />
+            <Label htmlFor="new-client-name" className="mb-2">Novo nome</Label>
+            <Input id="new-client-name" />
           </div>
           <DialogFooter className="flex-row">
-            <Button className="w-1/2" onClick={ () => setOpen(false) }>Cancelar</Button>
-            <Button className="w-1/2" onClick={ () => setOpen(false) }>Confirmar</Button>
+            <Button className="w-1/2" onClick={ () => setOpenEditClient(false) }>Cancelar</Button>
+            <Button className="w-1/2" onClick={ () => setOpenEditClient(false) }>Confirmar</Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
-    </div>
+
+      <Dialog open={ openAddClient } onOpenChange={ setOpenAddClient }>
+        <DialogTrigger />
+
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Adicionar cliente</DialogTitle>
+          </DialogHeader>
+          <div>
+            <Label htmlFor="client-name" className="mb-2">Nome</Label>
+            <Input id="client-name" />
+          </div>
+          <DialogFooter className="flex-row">
+            <Button className="w-1/2" onClick={ () => setOpenAddClient(false) }>Cancelar</Button>
+            <Button className="w-1/2" onClick={ () => setOpenAddClient(false) }>Confirmar</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
