62e94b323dedc6f0a4daa22a5ab67346ff7c4d3f
Leticia Cosmo <letcosmogeller@gmail.com>
2025-07-08 22:26:42 -0300

    setup vitest

---
+++ util/tests/math.test.ts
@@ -0,0 +1,21 @@
+import { toNegative, toPositive } from "../math"
+
+describe("Test toPositive function", ( )=> {
+  it("When given a negative number then return it's absolute value", ()=>{
+    expect(toPositive(-1)).toBe(1); 
+  })
+
+  it("When given a positive number then return it's absolute value", ()=>{
+    expect(toPositive(1)).toBe(1); 
+  })
+})
+
+describe("Test toNegative function", ( )=> {
+  it("When given a positive number then return it's absolute value", ()=>{
+    expect(toNegative(1)).toBe(-1); 
+  })
+
+  it("When given a negative number then return the same value", ()=>{
+    expect(toNegative(-1)).toBe(-1); 
+  })
+})
