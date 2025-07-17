4c5bfafaacdb605ac61bba49e98254f64f856d66
Leticia Cosmo <letcosmogeller@gmail.com>
2025-07-17 20:26:04 -0300

    added homepage test file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'), // ou 'app' se o alias for para dentro de app/
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom', 
